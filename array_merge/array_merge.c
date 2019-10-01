cd48e755a65ebcf4398f8ba2874a3552ce94798f
unicornwizard375 <drumm040@moris.umn.edu>
2019-10-01 12:40:48 -0500

    We have freed all and have zero leaks. We still have 26 context errors at this point.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,51 @@
 #include "array_merge.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "../mergesort/mergesort.h"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+	int m = num_arrays;
+	int sum =0;
+	int startindex=0;
+	int size=0;
+	int count=1;
+
+	for(int i=0; i<m;++i){
+	sum+=sizes[i];	
+	}
+
+	int* sort = (int*) calloc(sum,sizeof(int));
+	
+	for(int i=0; i<m;i++){
+		for(int j=0; j<sizes[i];j++){
+			sort[j+startindex] = values[i][j];
+		}
+		startindex+=sizes[i];	
+	}
+	mergesort(sum,sort);
+	for(int i=0; i<sum;i++){
+		if(sort[i] != sort[i+1]){
+		size++;
+		}
+	}
+	int* final = (int*) calloc(size,sizeof(int));
+	final[0]=size;
+
+	for(int i=0; i<sum;i++){
+                if(sort[i] != sort[i+1]){
+                	final[count]=sort[i];
+			count++;
+                }
+        }
+	free(sort);
+
+	/*  printf("FinalArray contents [ ");
+
+	for(int i=0; i<size+1;i++){
+		printf(" %d ", final[i]);
+	}
+	printf(" ]");*/
+
+  return final;
 }
+
